ab438b81ab830310dc624e31e1969536cfc5ed30
BK <3031011@naver.com>
2024-03-31 20:34:42 +0900

    feat<theme>: styled-components 글로벌 테마 생성

---
+++ src/components/ui/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components/dist/types';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#000',
+    secondary: '#fff',
+  },
+};
+
+export default theme;
