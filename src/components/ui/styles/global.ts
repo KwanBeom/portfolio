62aa2a8f74fee58c814e01f14aca5a7893ea751e
BK <3031011@naver.com>
2024-03-31 10:23:44 +0900

    style<GlobalStyle>: 글로벌 스타일 정의

---
+++ src/components/ui/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+import resetCss from 'styled-reset';
+
+export default createGlobalStyle`
+${resetCss}
+
+
+@font-face {
+  font-family: 'Pretendard-Regular';
+  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+    format('woff');
+  font-weight: 400;
+  font-style: normal;
+}
+
+:root {
+  --main-color: #000;
+  --text-color: #fff;
+  --title-font-size: clamp(1rem, 4vw + 1rem, 10rem);
+  --content-font-size: clamp(1rem, 2vw + 1rem, 8rem);
+}
+
+body {
+  margin: 0;
+  overflow: hidden;
+  -ms-overflow-style: none;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+html {
+  font-size: 10px;
+  font-family: 'Pretendard-Regular';
+}
+`;
