449dfd60c42888a0cc63dfaba67f647195644c8d
BK <3031011@naver.com>
2024-03-31 10:23:56 +0900

    chore<config>: 절대 경로 import 사용할 수 있도록 개츠비 설정 파일 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,27 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `web-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          components: 'src/components',
+          images: 'src/images',
+        },
+        extensions: ['.mjs', '.js', '.jsx', '.wasm', '.json', '.ts', '.tsx'],
+      },
+    },
+  ],
 };
 
 export default config;
