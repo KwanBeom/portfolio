1127f6d5b0bbb5a6ae5d7ab743e5e214ea8a8580
BK <3031011@naver.com>
2024-04-02 00:01:38 +0900

    feat<useZoomIn>: 줌인 애니메이션을 사용할 수 있는 커스텀 훅 구현

---
+++ src/hooks/animations/useZoomIn.ts
@@ -0,0 +1,44 @@
+import { AnimationHookProps } from './AnimationHookProps.type';
+
+type ZoomInProps<T> = AnimationHookProps<T> & {
+  zoomScale?: number;
+};
+
+const useZoomIn = <T extends HTMLElement>({ ref, mode, zoomScale = 1.2 }: ZoomInProps<T>) => {
+  const restore = () => {
+    if (!ref.current) return;
+    ref.current.animate([{ opacity: 1 }], { fill: 'forwards' });
+  };
+
+  const animations = {
+    trigger: (during: number) => {
+      if (!ref.current) return;
+      const keyframe: Keyframe[] = [
+        {
+          transform: `scale(${zoomScale})`,
+        },
+      ];
+      const animateOptions: KeyframeAnimationOptions = {
+        duration: during,
+        fill: 'forwards',
+      };
+
+      ref.current.animate(keyframe, animateOptions);
+    },
+
+    percentage: (percentage: number) => {
+      if (!ref.current || percentage > 100) return;
+      const onePercentScale = (zoomScale - 1) / 100;
+
+      ref.current.style.transform = `scale(${1 + onePercentScale * percentage})`;
+    },
+  };
+
+  return {
+    ref,
+    trigger: animations[mode],
+    restore,
+  };
+};
+
+export default useZoomIn;
