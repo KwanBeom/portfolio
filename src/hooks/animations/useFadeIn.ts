f3ffcc72cd634923317a24f68655fa31b31fa06e
BK <3031011@naver.com>
2024-04-02 00:02:11 +0900

    feat<useFadeIn>: 페이드인 애니메이션을 사용할 수 있는 커스텀 훅 구현

---
+++ src/hooks/animations/useFadeIn.ts
@@ -0,0 +1,40 @@
+import { AnimationHookProps } from './AnimationHookProps.type';
+
+const useFadeIn = <T extends HTMLElement>({ ref, mode }: AnimationHookProps<T>) => {
+  const restore = () => {
+    if (!ref.current) return;
+    ref.current.animate([{ opacity: 0 }], { fill: 'forwards' });
+  };
+
+  const animations = {
+    trigger: (during: number) => {
+      if (!ref.current) return;
+
+      const keyframe: Keyframe[] = [{ opacity: 1, visibility: 'visible' }];
+      const animateOptions: KeyframeAnimationOptions = {
+        duration: during,
+      };
+
+      setTimeout(() => {
+        ref.current!.style.opacity = '1';
+        ref.current!.style.visibility = 'visible';
+      }, during);
+
+      ref.current.animate(keyframe, animateOptions);
+    },
+
+    percentage: (percentage: number) => {
+      if (!ref.current || percentage > 100) return;
+
+      ref.current.style.opacity = String(1 - (100 - percentage) * 0.01);
+    },
+  };
+
+  return {
+    ref,
+    trigger: animations[mode],
+    restore,
+  };
+};
+
+export default useFadeIn;
