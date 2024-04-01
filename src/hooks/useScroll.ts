12c738fe458716671aad379d4b9457c3f70e1cd1
BK <3031011@naver.com>
2024-04-01 21:11:07 +0900

    refactor<useScroll>: useScroll 커스텀 훅 throttle 최적화 제거

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
-import { throttle } from 'lodash';
 
 const useScroll = () => {
   const [state, setState] = useState({ x: window.scrollX, y: window.scrollY });
 
-  const onScroll = throttle(() => {
+  const onScroll = () => {
     setState({ x: window.scrollX, y: window.scrollY });
-  }, 100);
+  };
 
   useEffect(() => {
     window.addEventListener('scroll', onScroll);
 
     return () => {
       window.removeEventListener('scroll', onScroll);
-      onScroll.cancel();
     };
   }, []);
 
