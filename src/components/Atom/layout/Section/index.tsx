7387f43d2f5a1a226775cea14be9d542d306dc91
BK <3031011@naver.com>
2024-04-03 18:44:51 +0900

    chore<Section>: 디렉토리명 변경
    components/Atom/template -> components/Atom/layout

--- src/components/Atom/layout/Section/index.tsx
+++ src/components/Atom/layout/Section/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Font from 'components/Atom/Font';
+import Font from 'components/atom/Font';
 import * as S from './Section.style';
 import { SectionTemplateProps } from './Section.type';
 
 const SectionTemplate = ({ title, children }: SectionTemplateProps) => {
   return (
     <S.Section>
-      <Font.Title color="point">{title}</Font.Title>
+      <Font.Title color="point" fontWeight="700">
+        {title}
+      </Font.Title>
       {children}
     </S.Section>
   );
