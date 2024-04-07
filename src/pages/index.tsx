0007bb2c69fc8ba926f313a23c9b9e13669d5b24
BK <3031011@naver.com>
2024-04-07 20:46:21 +0900

    feat<index>: 인덱스 페이지 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
+import GlobalStyle from 'components/ui/styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from 'components/ui/styles/theme';
+import Profile from 'components/synthesize/Profile';
+import Landing from 'components/synthesize/Landing';
+import Projects from 'components/synthesize/Projects';
+import Experiences from 'components/synthesize/Experiences';
 
 const IndexPage: React.FC<PageProps> = () => {
-  const test = 'a';
-  return <p>hello wolrd</p>;
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Landing />
+        <Profile />
+        <Projects />
+        <Experiences />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>최범관 포트폴리오</title>;
