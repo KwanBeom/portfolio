d31b657dbd90dcb99d364829c3d09d95e6ccabc6
BK <3031011@naver.com>
2024-04-07 12:17:20 +0900

    chore<Section>: 섹션 레이아웃 padding, gap 수정

--- src/components/Atom/layout/Section/Section.style.ts
+++ src/components/Atom/layout/Section/Section.style.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Section = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 2vh;
+  gap: 2rem;
   max-width: 980px;
   margin: 0 auto;
-  padding: max(2rem, 2vw);
+  padding: 3rem 2rem;
 `;
