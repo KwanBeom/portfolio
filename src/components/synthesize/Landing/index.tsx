9af74e4afcd9081861e9b8e012b419a810c6b6b4
BK <3031011@naver.com>
2024-04-07 18:35:33 +0900

    feat<Landing>: 랜딩 페이지 구현

---
+++ src/components/synthesize/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greet from 'components/organism/Greet';
+import ScrollActivateIntro from 'components/organism/ScrollActivateIntro';
+
+const Landing = () => {
+  return (
+    <>
+      <Greet />
+      <ScrollActivateIntro />
+    </>
+  );
+};
+
+export default Landing;
